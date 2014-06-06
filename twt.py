7f1dd25be1e8da35b9e9fd553c92461f81957ad1
Kristian Lauszus <lauszus@gmail.com>
2014-06-06 21:58:20 +0200

    Combined all python files

--- twt.py
+++
@@ -1,7 +0,0 @@
-# see birdy documentation for further use
-
-from birdy.twitter import UserClient
-client = UserClient('XXX',
-                    'XXX',
-                    'XXX',
-                    'XXX')
